d28ef4f8ced71268781fa5d6b362f0910f3b24ff
ankit <ankeetsinha@yahoo.com>
2017-01-28 18:55:46 +0530

    updated installation script and compose link

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
-#author "ankit"
-echo "this will install docker in ubuntu $(lsb_release -r -s)"
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates -y
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-#need to add conditions here so it can work on any ubuntu version
+#author strider
 rel=$(lsb_release -r -s)
-#checking ubuntu version and adding repo list only works with version >=14.04 & 16.04
-#need to add this  test condition here
-#if [ $rel >= 14.04 && $rel <= 16.04 ]
-#then
-case $rel in
-	16.04 )
-		echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-		;;
-	15.10 )
-		echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | sudo tee /etc/apt/sources.list.d/docker.list
-		;;
-	14.04 )
-		echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
-		;;
-esac
-#else 
-	#echo " docker works better with ubuntu version between 14.04 and 16.04"
-#fi
+if  [[ ( $rel != 16.10 && $rel != 16.04 && $rel != 14.04 ) ]] ; then
+	echo "To install Docker, you need the 64-bit version of one of these Ubuntu versions:16.10,16.04 (LTS),14.04 (LTS)"
+	exit 1
+else
+	echo "this will install docker in ubuntu $(lsb_release -r -s)"
+fi
+sudo apt-get install curl \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual \
+    apt-transport-https \
+    ca-certificates
+echo "adding docker GPG key"
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
 sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
-sudo apt-get install docker-engine -y
-sudo service docker start
-sudo docker run hello-world
-echo "installing docker-cmpose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo apt-get -y install docker-engine
+echo "installing docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-echo "adding docker group to your current user"
+echo "adding docker to user group"
 sudo usermod -aG docker $USER
+echo "configuring docker to start on boot-time"
+sudo systemctl enable docker
 echo "do you need to reboot"
 echo "press y/Y for yes"
 #asking for user permission to reboot , only Y/y should work
@@ -42,6 +34,6 @@ read -p "" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	exec  sudo /sbin/reboot > /dev/null 
+	exec /sbin/reboot > /dev/null
     #sudo reboot && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
